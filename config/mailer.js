17479a6538796deb80cbff8527ce4249f57e61de
Dhruv05-hue <dhruvpawar31205@gmail.com>
2026-07-28 09:40:23 +0530

    just

--- config/mailer.js
+++ config/mailer.js
@@ -1,18 +1,16 @@
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // <-- Change this to false for port 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
-
 
 module.exports = transporter;
